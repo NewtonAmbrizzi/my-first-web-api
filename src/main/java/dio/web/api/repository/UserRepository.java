6041a3709828e8b1e44541f051319d50737a104b
NewtonAmbrizzi <newton.ambrizzi@gmail.com>
2023-04-22 02:09:17 -0300

    Handler e exceptions

--- src/main/java/dio/web/api/repository/UserRepository.java
+++ src/main/java/dio/web/api/repository/UserRepository.java
@@ -5,11 +5,15 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import dio.web.api.handler.BusinessException;
 import dio.web.api.model.Usuario;
 
 @Repository
 public class UserRepository {
     public void save(Usuario usuario){
+        if (usuario.getLogin() == null) {
+            throw new BusinessException("O campo login é obrigatório!")
+        }
         if (usuario.getId()==null) {
             System.out.println("SAVE - Recebendo o usuário na camada de repositório");
         } else {
